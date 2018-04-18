07c01527f51b321ada9ed05c5fbb8332ac3716fa
Stanislav Vasic <e01327667@student.tuwien.ac.at>
2018-04-18 19:18:52 +0200

    moveable im svg

--- scripts/init.js
+++ scripts/init.js
@@ -11,23 +11,32 @@ $(document).ready(function() {
 
     // TODO init: add drag functionality to devices in sidebar
     $(".device").draggable({
-        revert: "invalid", helper: function (event) {
-            var clone = $(this).find(".device-image").clone();
-            return clone;
+        revert: "invalid",
+        cursor: "move",
+        cursorAt: { top: 50, left: 45 },
+        helper: function (event) {
+            return $('<img width="100" src= "'+$(this).find("img").attr("src")+'" alt= "3D- Drucker" > ')
         }
     });
 
     //$("diagram").droppable();
     $("#diagram").droppable({
+        accept: '.device',
         drop: function (event, ui) {
-            var clone = $(ui.helper).clone();
+          //  var clone = $(ui.helper).clone();
+            var clone = $(ui.draggable).clone();
             var parentOffset = $(this).parent().offset(); 
             var relX = event.pageX - parentOffset.left - 50;
-            var relY = event.pageY - parentOffset.top - 100;
+            var relY = event.pageY - parentOffset.top - 120;
             clone.attr("style", "position: absolute; left: " + relX + "px; top: " + relY+"px;");
             clone.attr("id", "bla2");
+            clone.removeClass();
+            $(clone).draggable();
 
             $(this).find(".devices").append(clone);
+
         }
     });
+    
+
 });
